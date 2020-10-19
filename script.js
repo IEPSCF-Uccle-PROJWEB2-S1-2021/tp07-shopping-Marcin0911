e8279cf72ed6435656ed1aa9e2ff7af36aa80d64
Marcin <m_a_r_c_i_n_@hotmail.com>
2020-10-19 13:14:25 +0200

    DOM

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // Écrivez votre code JavaScript ici.
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
+
+const items = () => {
+
+  let li = document.createElement("li");
+  let span = document.createElement("span");
+  let btn1 = document.createElement("button");
+  span = (document.createTextNode(input.value));
+  li.appendChild(span);
+  btn1.onclick = function() {
+    ul.removeChild(li);
+  };
+  li.appendChild(btn1);
+  btn1 = (btn1.innerHTML = "Delete");
+  ul.appendChild(li);
+  input.value = "";
+  input.focus();
+
+}
+
+btn.addEventListener("click", items);
+
